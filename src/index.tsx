120e5e326dfe35b19aef3b3a69eb89bb9b578f34
wb-zqy582530 <wb-zqy582530@alibaba-inc.com>
2019-10-30 17:56:24 +0800

    update: update main file

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WaterMark extends React.Component {
+  render() {
+    debugger;
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
+export default WaterMark;
